998926710e20744fb723956dd2991c504aca0808
Kenton Lewis <K-Bird@users.noreply.github.com>
2018-09-02 15:50:26 -0400

    Program Record - Current Date
    If game date is before on today's date count the game toward the overall record. If not, don't count it

--- libs/ajax/calc_program_record.php
+++ libs/ajax/calc_program_record.php
@@ -15,16 +15,26 @@ $ties = 0;
 $getGames = db_query("SELECT * FROM `games` WHERE Season_ID >= '{$startYearID}' AND Season_ID <= '{$endYearID}'");
 
 while ($fetchGames = $getGames->fetch_assoc()) {
-    if ($fetchGames['OSU_Score'] > $fetchGames['Opp_Score']) {
-        $wins++;
-    }
-    if ($fetchGames['OSU_Score'] < $fetchGames['Opp_Score']) {
-        $losses++;
-    }
-    if ($fetchGames['OSU_Score'] === $fetchGames['Opp_Score']) {
-        $ties++;
+    
+    $getGameDate = $fetchGames['Date'];
+    $explodeGameDate = explode("/", $getGameDate);
+    $GameDate = $explodeGameDate[2].'-'.$explodeGameDate[0].'-'.$explodeGameDate[1];
+    $todayDate = date('Y-m-d');
+    
+    if ($GameDate >= $todayDate) {
+        
+    } else {
+        if ($fetchGames['OSU_Score'] > $fetchGames['Opp_Score']) {
+            $wins++;
+        }
+        if ($fetchGames['OSU_Score'] < $fetchGames['Opp_Score']) {
+            $losses++;
+        }
+        if ($fetchGames['OSU_Score'] === $fetchGames['Opp_Score']) {
+            $ties++;
+        }
     }
 }
 
 
-echo 'Record During Displayed Years: '. $wins . ' Wins - ' . $losses . ' Losses - ' . $ties . ' Ties';
+echo 'Record During Displayed Years: ' . $wins . ' Wins - ' . $losses . ' Losses - ' . $ties . ' Ties';
