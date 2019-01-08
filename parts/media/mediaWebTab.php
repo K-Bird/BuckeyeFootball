887696db954d1dbf3b7b2cb0be191d9faf9a4631
Kenton Lewis <K-Bird@users.noreply.github.com>
2019-01-08 11:10:26 -0500

    Web Links
    - Display table of web links

--- parts/media/mediaWebTab.php
+++ parts/media/mediaWebTab.php
@@ -1,1 +1,26 @@
-Web Tab
+<div class="row">
+    <div class="col-lg-12">
+        <br><br>
+        <table id="webLinksTable" class="table-sm table-hover text-center tablesorter">
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Category</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody class="list">
+                <?php
+                $getLinks = db_query("SELECT * FROM `web_links`");
+                while ($fetchLinks = $getLinks->fetch_assoc()) {
+                    echo '<tr>';
+                    echo '<td class="web-sort-title">',$fetchLinks['Title'],'</td>';
+                    echo '<td class="web-sort-cat">',$fetchLinks['Category'],'</td>';
+                    echo '<td><a href="',$fetchLinks['URL'],'" target="_blank">Link</a></td>';
+                    echo '</tr>';
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+</div>
