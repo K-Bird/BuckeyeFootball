2c3ce0adaa3f19007bf35b2620f2ddb7ee65f357
Kenton Lewis <K-Bird@users.noreply.github.com>
2019-03-25 08:25:44 -0400

    data.php cleanup
    - Consistancy check
    - Comments check
    - Format check

--- data.php
+++ data.php
@@ -1,8 +1,10 @@
 <html>
     <head>
-        <title>OSU - Stats</title>
+        <title>Buckeyes - Data</title>
+        <link rel="shortcut icon" href="http://www.iconj.com/ico/y/f/yfuwmmd6a8.ico" type="image/x-icon" />
         <link rel="stylesheet" type="text/css" href="libs/css/bootstrap.css">
         <link rel="stylesheet" type="text/css" href="libs/css/nouislider.css">
+        <link rel="stylesheet" type="text/css" href="libs/css/open-iconic-bootstrap.css">
         <link rel="stylesheet" type="text/css" href="libs/css/common.css">
         <script src="libs/js/jquery.js"></script>
         <script src="libs/js/bootstrap.js"></script>
@@ -11,9 +13,11 @@
         <script src="libs/js/commonFunctions.js"></script>
     </head>
     <body>
-            <?php include ('nav/navBar.php'); ?>
-        <div class="container">
-            <h1>Under Construction</h1>
+        <?php include ('nav/navBar.php'); ?>
+        <div class="container" style="text-align: center">
+            <br>
+            <span class="oi oi-lock-locked"></span>
+            <h3>Under Construction</h3>
         </div>
     </body>
 </html>
