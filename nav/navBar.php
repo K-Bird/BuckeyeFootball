e0ffa2184a7d83aff4eac9b36026b98eb45af521
Kenton-Dev-Office <K-Bird@users.noreply.github.com>
2020-12-15 15:53:30 -0500

    Environment ID
    - Added Environment Indicator on NavBar
    - Ignoring the local file with the environment text

--- nav/navBar.php
+++ nav/navBar.php
@@ -1,17 +1,18 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#"><img src="libs/images/logo_nav.png" height="55" width="55"></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-item nav-link" href="index.php">Home</a>
-      <a class="nav-item nav-link" href="seasons.php">Seasons</a>
-      <a class="nav-item nav-link" href="recruits.php">Recruits</a>
-      <a class="nav-item nav-link" href="players.php">Players</a>
-      <a class="nav-item nav-link" href="data.php">Data</a>
-      <a class="nav-item nav-link" href="media.php">Media</a>
+    <a class="navbar-brand" href="#"><img src="libs/images/logo_nav.png" height="55" width="55"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+            <a class="nav-item nav-link" href="index.php">Home</a>
+            <a class="nav-item nav-link" href="seasons.php">Seasons</a>
+            <a class="nav-item nav-link" href="recruits.php">Recruits</a>
+            <a class="nav-item nav-link" href="players.php">Players</a>
+            <a class="nav-item nav-link" href="data.php">Data</a>
+            <a class="nav-item nav-link" href="media.php">Media</a>
+        </div>
     </div>
-  </div>
-  <a class="nav-item nav-link" href="input.php">Input Data</a>
+    <?php include ('nav/nav_id.php'); ?>
+    <a class="nav-item nav-link" href="input.php">Input Data</a>
 </nav>
